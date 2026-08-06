45cd9acd0cc7eedb639ee21d51f69767736fb21b
honebuto-3010 <blacknile66@yahoo.co.jp>
2026-08-06 11:10:03 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,19 +10,20 @@ const urlsToCache = [
 ];
 
 // インストール（初回キャッシュ）
-self.addEventListener("install", event => {
+//self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
     })
   );
-});
+//});
 
 // オフライン時はキャッシュを返す
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      if (response) return response;
+      return fetch(event.request);
     })
   );
 });
